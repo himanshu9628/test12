02bfc125d5c6caf7ebc3e81f397265c3c78a935d
himansu <60183595+himanshu9628@users.noreply.github.com>
2022-11-04 15:05:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 # from turtle import update
 
 # import imp
-from flask import Flask, jsonify,render_template,request,redirect,url_for,session
+from flask import Flask, jsonify,render_template,request,redirect,url_for,session,send_file
 # from flask_restful import Resource, Api
 from datetime import datetime
 import json,requests,random
@@ -20,6 +20,20 @@ def path():
     print(app.root_path)
     return app.root_path
 
+@app.route('/download')
+def downloadFile ():
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    path = app.root_path+"/project1.db"
+    return send_file(path, as_attachment=True)
+
+
 @app.route('/')
 def home():
     return "Welcome"
+
+
+
+
+if __name__ == '__main__':
+  
+    app.run(debug = True)
